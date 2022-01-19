541ed94133d04f13c732cb129b906e7716761bd1
name-era <mkuwana@keio.jp>
2022-01-19 23:11:39 +0900

    implemet gltf loader func

--- Skinning/Src/gltf/gltfLoader.cpp
+++ Skinning/Src/gltf/gltfLoader.cpp
@@ -2,7 +2,29 @@
 #include <iostream>
 
 cgltf_data* LoadGLTFFile(const char* path) {
+	cgltf_options options;
+	memset(&options, 0, sizeof(cgltf_options));
+	cgltf_data* data = NULL;
 
+	cgltf_result result = cgltf_parse_file(&options, path, &data);
+	if (result != cgltf_result_success) {
+		std::cout << "could not load input file.\n";
+		return 0;
+	}
+
+	result = cgltf_load_buffers(&options, data, path);
+	if (result != cgltf_result_success) {
+		std::cout << "could not load buffers.\n";
+		return 0;
+	}
+
+	result = cgltf_validate(data);
+	if (result != cgltf_result_success) {
+		std::cout << "invalid gltf file.\n";
+		return 0;
+	}
+
+	return data;
 }
 
 
