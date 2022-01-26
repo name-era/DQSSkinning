8a346e7a8246a92f9d451bd79531a39249589e42
name-era <mkuwana@keio.jp>
2022-01-26 19:11:51 +0900

    pose, transform クラスを追加

---
+++ Skinning/Src/animation/pose.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <vector>
+#include "transform.h"
+
+class Pose {
+protected:
+	std::vector<Transform>	_joints;
+	std::vector<uint32_t> _parents;
+
+public:
+	/**
+	* コンストラクタ
+	*/
+	Pose();
+	Pose(uint32_t numJoiints);
+
+	/**
+	* ジョイントの数の変更
+	*/
+	void Resize(uint32_t size);
+
+	/**
+	* コピーコンストラクタ
+	*/
+	Pose(const Pose& p);
+
+	/**
+	* 代入演算子
+	*/
+	Pose& operator=(const Pose& p);
+
+	/**
+	* サイズを取得する
+	*/
+	uint32_t GetSize();
+
+	/**
+	* ローカルトランスフォームをセットする
+	*/
+	void SetLocalTransform(uint32_t index, const Transform& transform);
+
+	/**
+	* ローカルトランスフォームを取得する
+	*/
+	Transform GetLocalTransform(uint32_t index);
+	
+	/**
+	* グローバルトランスフォームを取得する
+	*/
+	Transform GetGlobalTransform(uint32_t index);
+
+	Transform operator[](uint32_t index);
+
+	void GetMatrixPalette(std::vector<glm::mat4>& out);
+
+	/**
+	* 親をセットする
+	*/
+	void SetParent(uint32_t index, uint32_t parent);
+
+	/**
+	* 親を取得する
+	*/
+	uint32_t GetParent(uint32_t index);
+
+	bool operator==(const Pose& other);
+	bool operator!=(const Pose& other);
+};
